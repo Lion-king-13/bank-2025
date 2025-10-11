94128582683cc09b1ba3b983d9a414bd7f634187
Lion <damien.crappe13@outlook.com>
2025-10-11 12:47:59 +0200

    2 sans methode

--- damien crappe/bank/Program.cs
+++ damien crappe/bank/Program.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Dynamic;
+using System.Globalization;
 
 class Person
 {
     public string FirstName;
     public string LastName;
     public DateTime BirthDate;
+}
+class CurrentAccount
+{
+    public string Number;
+    public double Balance { get; }
+    public double CreditLine;
+    public Person Owner;
 }
