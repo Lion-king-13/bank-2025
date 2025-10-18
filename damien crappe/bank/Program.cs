7b6d11ce53168d12c1add8ab776530c173f8e178
Lion <damien.crappe13@outlook.com>
2025-10-18 09:33:20 +0200

    exo 1-5

--- damien crappe/bank/Program.cs
+++ damien crappe/bank/Program.cs
@@ -14,4 +14,45 @@ class CurrentAccount
     public double Balance { get; }
     public double CreditLine;
     public Person Owner;
+    public void Deposit(double amount, double Balance)
+    {
+        Balance += amount;
+    }
+    public void Withdraw(double amount, double Balance)
+    {
+        Balance -= amount;
+    }
+}
+class Bank
+{
+    public readonly Dictionary<string, CurrentAccount> Accounts = [];
+    public string Name;
+    public void AddAccount(CurrentAccount account)
+    {
+        Accounts[account.Number] = account;
+    }
+    public void RemoveAccount(string accountNumber)
+    {
+        Accounts.Remove(accountNumber);
+    }
+
+}
+class Methods
+{
+    public void ShowMoney(CurrentAccount account)
+    {
+        Console.WriteLine($"Le compte {account.Number} a un solde de {account.Balance} euros.");
+    }
+    public void ShowMoneySum(Bank bank, Person owner)
+    {
+        double SumAccount = 0;
+        foreach (var account in bank.Accounts.Values)
+        {
+            if (account.Owner == owner)
+            {
+                SumAccount += account.Balance;
+            }
+        }
+        Console.WriteLine($"La somme des comptes de {owner.FirstName} {owner.LastName} est de {SumAccount} euros.");
+    }
 }
